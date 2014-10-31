0c70abe429d60c993e53e98deefe84c6b1758434
Victor Rivas <vctr90w@gmail.com>
2014-10-31 17:49:58 -0430

    Creating products pages

--- css/css_handler.js
+++ css/css_handler.js
@@ -40,3 +40,27 @@ $('header .main_link').on('click',function(){
 		$('header ul').hide();
 	}
 });
+
+var sub_menu_movil = {
+	variables:{
+		hidden:true
+	},
+	css:{
+		show_hide_sub_menu:function(){
+			if(sub_menu_movil.variables.hidden){
+				$('.sub-menu').removeClass('hidden');
+				$('.selected').removeClass('selected');
+				$('#productos-menu').addClass('selected');
+				sub_menu_movil.variables.hidden = false;
+			}else{
+				$('.sub-menu').addClass('hidden');
+				$('.selected').removeClass('selected');
+				sub_menu_movil.variables.hidden = true;
+			}
+		},
+	}
+}
+
+$('#productos-menu').on('click',function(){
+	sub_menu_movil.css.show_hide_sub_menu();
+});
